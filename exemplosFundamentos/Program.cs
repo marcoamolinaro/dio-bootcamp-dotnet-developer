f1c7fd0290398563f529d40157207183856d149c
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-03-06 17:13:59 -0300

    First Commit

---
+++ exemplosFundamentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using exemplosFundamentos.Models;
+
+// See https://aka.ms/new-console-template for more information
+/* Console.WriteLine("Hello, World!");
+Pessoa pessoa = new Pessoa();
+pessoa.Nome = "Marco";
+pessoa.Idade = 59;
+pessoa.Apresentar(); */
+
+double altura = 1.80;
+decimal preco = 1.80M;
+
+Console.WriteLine("Altura " + altura);
+Console.WriteLine("Altura " + altura.ToString("0.00"));
+Console.WriteLine("Preço " + preco);
